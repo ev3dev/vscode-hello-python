77a0d140b33d7213efe4aa8b1bfab2ed1ce69c71
David Lechner <david@lechnology.com>
2017-08-15 20:25:28 -0500

    Add a hello world program

---
+++ hello.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+'''Hello to the world from ev3dev.org'''
+
+import os
+import sys
+import time
+
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
+
+def reset_console():
+    '''Resets the console to the default state'''
+    print('\x1Bc', end='')
+
+# state constants
+ON = True
+OFF = False
+
+def set_cursor(state):
+    '''Turn the cursor on or off'''
+    if state:
+        print('\x1B[?25h', end='')
+    else:
+        print('\x1B[?25l', end='')
+
+def set_font(name):
+    '''Sets the console font
+
+    A full list of fonts can be found with `ls /usr/share/consolefonts`
+    '''
+    os.system('setfont ' + name)
+
+def main():
+    '''The main function of our program'''
+
+    # set the console just how we want it
+    reset_console()
+    set_cursor(OFF)
+    set_font('Lat15-Terminus18')
+
+    # print something to the screen of the device
+    print('Hello World!')
+
+    # print something to the output panel in VS Code
+    debug_print('Hello VS Code!')
+
+    # wait a bit so you have time to look at the display before the program exits
+    time.sleep(5)
+
+if __name__ == '__main__':
+    main()
